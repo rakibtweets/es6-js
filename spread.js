831d6d2f53a4fefee5b9fdb6ebcbbcfdcea338c4
rakibtweets <rakibhasanroky0@gmail.com>
2021-08-24 04:52:02 +0600

    es6 syntex js

--- spread.js
+++ spread.js
@@ -18,7 +18,10 @@ const maxInArray = Math.max(...numbers) // output biggest number of array
 // console.log(numbers) // ager numbers a push hobe beacuse of reference variable
 
 // what is reference variable?
-/* => jodi ekta variable er value amra onno arekta variable a boshai tokhon new variable first variable theke reference ney.
+/* => jodi ekta variable er value amra onno arekta variable a boshai tokhon new variable first variable theke reference ney. 
  */
 
 // pushing new  numbers in an array
+const newNumbers =[34,234,...numbers,65,70];
+newNumbers.push(45)
+console.log(newNumbers) 
